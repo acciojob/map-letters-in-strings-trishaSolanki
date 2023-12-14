cca176710b88d714ff501787efa1f212547c4b89
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-14 16:27:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(word) {
+  const letterMap = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    if (!letterMap.hasOwnProperty(letter)) {
+      // If the letter is not already a key in the object, initialize it as an array
+      letterMap[letter] = [];
+    }
+    // Push the index to the array corresponding to the letter
+    letterMap[letter].push(i);
+  }
+
+  return letterMap;
+}
